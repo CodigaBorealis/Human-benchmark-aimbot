7700fde04832cf70655c06e284352c95758c5018
Xenire <135284394+CodigaBorealis@users.noreply.github.com>
2025-08-13 18:47:55 -0600

    added the stop condition
    the script should now stop once the test is over, the only thing left is lowering the runtime

--- aimbot.py
+++ aimbot.py
@@ -1,13 +1,15 @@
 import time
 import pyautogui
 import keyboard
+import sys
 active=True
-
+targets=0
 
 def checkScreen():#takes about 363ms to clear the test
     #needs optimization
     #either use numpy or use a while so the lookup is faster
     global active
+    global targets
     try:
         while(active):
             found=False
@@ -21,15 +23,18 @@ def checkScreen():#takes about 363ms to clear the test
                     print("script has been stopped")
                     break
             pixel=regionScreenshot.load()
-            for x in range(width):
-                for y in range(height):
+            for x in range(width):#this shit is the bottleneck
+                for y in range(height):#i should convert the pixel array onto a vector and use numpy to do matrix calculations
                     if pixel[x,y]==(149,195,232):
                             pyautogui.moveTo(486+x,181+y)
                             pyautogui.click(button="left",clicks=1)
                             found=True
                             break
                 if(found):
+                    targets+=1
                     break
+                if(targets==32):#32 because otherwise it ends 2 targets early
+                    sys.exit()
 
                         
                     
